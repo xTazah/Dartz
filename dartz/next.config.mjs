6ac0042da01915f638f17b0b70cde7cd89e6108f
xTazah <finnbeifong@gmail.com>
2024-12-04 16:46:04 +0100

    fix build error

--- dartz/next.config.mjs
+++ dartz/next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: './', // Use relative paths for assets
-    trailingSlash: true, // Ensure all paths end with a slash
   };
   
   export default nextConfig;
