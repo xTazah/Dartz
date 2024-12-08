e042ddba8115cd5a2e252b262aed60abb17c0159
xTazah <finnbeifong@gmail.com>
2024-12-08 18:36:14 +0100

    fix build error

--- dartz/src/app/lobby/page.tsx
+++ dartz/src/app/lobby/page.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Navigation from '../components/navigation/navigation'
 import LobbyComponent from '../components/lobby/lobby'
 
 export default function lobby() {
   return (
     <>
-        <div className='text-8xl text-indigo-900 cursor-pointer'>Lobby test</div>
+      <div className='text-8xl text-indigo-900 cursor-pointer'>Lobby test</div>
+      <Suspense>
         <LobbyComponent/>
+      </Suspense>
     </>
   )
 }
